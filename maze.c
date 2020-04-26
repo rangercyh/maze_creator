84c7870aba4261ad9339ddcad032497bb210d78b
caiyiheng <rangercyh@qq.com>
2020-04-26 18:50:18 +0800

    add expand

--- maze.c
+++ maze.c
@@ -14,6 +14,7 @@
 typedef struct link_node {
     int x;
     int y;
+    int dir;
     unsigned char m[0];
 } PATH_NODE;
 
@@ -66,12 +67,13 @@ static inline int check_in_map(int x, int y, int w, int h) {
     return x >= 0 && y >= 0 && x < w && y < h;
 }
 
-static PATH_NODE *construct(int x, int y, int w, int h, unsigned char *father_map) {
+static PATH_NODE *construct(int x, int y, int dir, int w, int h, unsigned char *father_map) {
     int map_len = w * h;
     int mem_len = BITSLOT(map_len) + 1;
     PATH_NODE *node = (PATH_NODE *)malloc(sizeof(PATH_NODE) + mem_len * sizeof(node->m[0]));
     node->x = x;
     node->y = y;
+    node->dir = dir;
     if (father_map == NULL) {
         memset(node->m, 0, mem_len * sizeof(node->m[0]));
     } else {
@@ -81,25 +83,32 @@ static PATH_NODE *construct(int x, int y, int w, int h, unsigned char *father_ma
     return node;
 }
 
+#define DIR_NUM 4
+#define EMPTY_DIR -1
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+
 static int *get_neighbor(int x, int y, int dir) {
     static int pos[2] = {0};
     switch (dir) {
-        case 1: { // up
+        case UP: {
             pos[0] = x;
             pos[1] = y - 1;
             break;
         }
-        case 2: { // down
+        case DOWN: {
             pos[0] = x;
             pos[1] = y + 1;
             break;
         }
-        case 3: { // left
+        case LEFT: {
             pos[0] = x - 1;
             pos[1] = y;
             break;
         }
-        case 4: { // right
+        case RIGHT: {
             pos[0] = x + 1;
             pos[1] = y;
             break;
@@ -108,69 +117,335 @@ static int *get_neighbor(int x, int y, int dir) {
     return pos;
 }
 
-static int *shuffe() {
+static void shuffe(int *r) {
     int i, s, c;
-    static int r[4] = { 1, 2, 3, 4 };
-    for (i = 0; i < 4; i++) {
-        r[i] = i + 1;
-    }
-    for (i = 4; i > 0; i--) {
+    for (i = DIR_NUM; i > 0; i--) {
         s = rand() % i;
         c = r[s];
         r[s] = r[i - 1];
         r[i - 1] = c;
     }
-    return r;
 }
 
 static int
 find_path(PATH_STACK *s, int w, int h) {
-    PATH_NODE *p = top(s);
+    PATH_NODE *p = top(s), *cur;
     if (p != NULL) {
-        int *arr = shuffe(), mark = 0;
-        for (int i = 0; i < 4; i++) {
-            int *pos = get_neighbor(p->x, p->y, arr[i]);
-            int nx = pos[0], ny = pos[1];
+        int *pos, nx, ny, mark = 0;
+        int arr[DIR_NUM] = { UP, DOWN, LEFT, RIGHT };
+        shuffe(arr);
+        for (int i = 0; i < DIR_NUM; i++) {
+            pos = get_neighbor(p->x, p->y, arr[i]);
+            nx = pos[0];
+            ny = pos[1];
+            // printf("check nei x = %d, y = %d, nx = %d, ny = %d, dir = %d\n", p->x, p->y, nx, ny, arr[i]);
             if (check_in_map(nx, ny, w, h) && !BITTEST(p->m, ny * w + nx)) {
-                PATH_NODE *cur = construct(nx, ny, w, h, p->m);
+                cur = construct(nx, ny, arr[i], w, h, p->m);
                 push(s, cur);
+                // printf("push create node x = %d, y = %d, nx = %d, ny = %d\n", p->x, p->y, nx, ny);
                 mark = 1;
                 if (full(s)) {
                     return 1;
                 }
                 if (find_path(s, w, h)) {
                     return 1;
+                } else {
+                    mark = 0;
                 }
             }
         }
         if (!mark) {
             PATH_NODE *p = pop(s);
             if (p != NULL) {
+                printf("pop node x = %d, y = %d\n", p->x, p->y);
                 free(p);
+                p = NULL;
             }
         }
     }
     return 0;
 }
 
+typedef struct candidate_node {
+    int x;
+    int y;
+    int dir_unit[DIR_NUM]; // UP DOWN LEFT RIGHT
+    int choose_dir;
+    struct candidate_node *next;
+} CANDIDATE_NODE;
+
+typedef struct candidate_list {
+    int num;
+    CANDIDATE_NODE *head;
+    CANDIDATE_NODE *tail;
+} CANDIDATE_LIST;
+
+typedef struct expand_list {
+    int num;
+    CANDIDATE_NODE *arr[0];
+} EXPAND_LIST;
+
+static int
+check_in_path(int x, int y, int w, int h, unsigned char *m) {
+    return check_in_map(x, y, w, h) && BITTEST(m, y * w + x);
+}
+
+static CANDIDATE_NODE *
+check_in_candidate(CANDIDATE_LIST *c, int x, int y) {
+    CANDIDATE_NODE *p = c->head;
+    while (p != NULL) {
+        if (p->x == x && p->y == y) {
+            return p;
+        }
+        p = p->next;
+    }
+    return NULL;
+}
+
+static int
+check_in_expand(EXPAND_LIST *l, int x, int y) {
+    for (int i = 0; i < l->num; i++) {
+        if (l->arr[i]->x == x && l->arr[i]->y == y) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+static CANDIDATE_NODE *
+construct_candidate(int x, int y, int dir, int dir_num) {
+    CANDIDATE_NODE *cp = (CANDIDATE_NODE *)malloc(sizeof(CANDIDATE_NODE));
+    cp->x = x;
+    cp->y = y;
+    cp->choose_dir = EMPTY_DIR;
+    cp->next = NULL;
+    memset(cp->dir_unit, 0, DIR_NUM * sizeof(int));
+    cp->dir_unit[dir] = dir_num + 1;
+    return cp;
+}
+
 static void
-back_tracking(lua_State *L, PATH_STACK *s) {
+push_candidate(CANDIDATE_LIST *c, CANDIDATE_NODE *p) {
+    c->num++;
+    if (c->head == NULL) {
+        c->head = c->tail = p;
+    } else {
+        c->tail->next = p;
+        c->tail = p;
+    }
+}
+
+static void
+search_candidate_neighbor(int x, int y, int w, int h, unsigned char *m,
+    CANDIDATE_LIST *c, EXPAND_LIST *l, int limit, int dir, int dir_num) {
+    int *pos;
+    int cx, cy;
+    for (int i = 0; i < DIR_NUM; i++) {
+        pos = get_neighbor(x, y, i);
+        cx = pos[0];
+        cy = pos[1];
+        if (dir == i && (dir_num + 1) > limit) {
+            break;
+        }
+        if (check_in_path(cx, cy, w, h, m)) {
+            break;
+        }
+        if (check_in_expand(l, cx, cy)) {
+            break;
+        }
+        CANDIDATE_NODE *p = check_in_candidate(c, cx, cy);
+        if (p != NULL) {
+            p->dir_unit[i] += 1;
+            break;
+        }
+        push_candidate(c, construct_candidate(cx, cy, i, ((i == dir) ? dir_num : 0)));
+    }
+}
+
+static CANDIDATE_LIST *
+search_for_candidate(PATH_STACK *s, EXPAND_LIST *l, int limit, int w, int h) {
+    CANDIDATE_LIST *c = (CANDIDATE_LIST *)malloc(sizeof(CANDIDATE_LIST));
+    c->num = 0;
+    c->head = c->tail = NULL;
+    PATH_NODE *p, *end = top(s);
+    for (int i = 1; i < s->top; i++) {
+        p = s->arr[i];
+        search_candidate_neighbor(p->x, p->y, w, h, end->m, c, l, limit, EMPTY_DIR, 0);
+    }
+    return c;
+}
+
+static void release_candidate(CANDIDATE_LIST *c) {
+    if (c != NULL) {
+        CANDIDATE_NODE *p;
+        while (c->head != c->tail) {
+            p = c->head;
+            c->head = c->head->next;
+            free(p);
+        }
+        if (c->head != NULL) {
+            free(c->head);
+        }
+        free(c);
+        c = NULL;
+    }
+}
+
+static int add_expand(EXPAND_LIST *l, CANDIDATE_NODE *p) {
+    // choose a dir
+    int total_dir = 0;
+    for (int i = 0; i < DIR_NUM; i++) {
+        if (p->dir_unit[i] > 0) {
+            total_dir++;
+        }
+    }
+    int idx = rand() % total_dir;
+    int dir_num = 0;
+    for (int j = 0; j < DIR_NUM; j++) {
+        if (p->dir_unit[j] > 0) {
+            idx--;
+            if (idx < 0) {
+                p->choose_dir = j;
+                dir_num = p->dir_unit[j];
+                break;
+            }
+        }
+    }
+    l->arr[l->num++] = p;
+    return dir_num;
+}
+
+static EXPAND_LIST *
+expand(PATH_STACK *s, int ex_num, int ex_limit, int w, int h) {
+    PATH_NODE *end = top(s);
+    if (end == NULL || ex_num <= 0 || ex_limit <= 0) {
+        return NULL;
+    }
+    EXPAND_LIST *l = (EXPAND_LIST *)malloc(sizeof(EXPAND_LIST) + ex_num * sizeof(CANDIDATE_NODE *));
+    l->num = 0;
+    printf("create l\n");
+    // CANDIDATE_LIST *c = search_for_candidate(s, l, ex_limit, w, h);
+    // int pos;
+    // CANDIDATE_NODE *p, *q;
+    // while (ex_num-- > 0) {
+    //     pos = rand() % c->num;
+    //     p = c->head;
+    //     if (pos == 0) {
+    //         c->head = c->head->next;
+    //         c->num--;
+    //     } else {
+    //         pos--;
+    //         while (pos-- > 0) {
+    //             p = p->next;
+    //         }
+    //         q = p->next;
+    //         p->next = p->next->next;
+    //         p = q;
+    //     }
+    //     int dir_num = add_expand(l, p);
+    //     search_candidate_neighbor(p->x, p->y, w, h, end->m, c, l, ex_limit, p->choose_dir, dir_num);
+    // }
+    // release_candidate(c);
+    return l;
+}
+
+static void release_expand(EXPAND_LIST *l) {
+    if (l != NULL) {
+        for (int i = 0; i < l->num; i++) {
+            free(l->arr[i]);
+            l->arr[i] = NULL;
+        }
+        free(l);
+        l = NULL;
+    }
+}
+
+static inline int
+check_max_bound(int cur, int bound) {
+    return cur > bound ? cur : bound;
+}
+
+static inline int
+check_min_bound(int cur, int bound) {
+    return cur < bound ? cur : bound;
+}
+
+static inline void
+add_lua_table(lua_State *L, int x, int y, int dir, int pos) {
     lua_newtable(L);
+    lua_pushinteger(L, x);
+    lua_rawseti(L, -2, 1);
+    lua_pushinteger(L, y);
+    lua_rawseti(L, -2, 2);
+    lua_pushinteger(L, dir);
+    lua_rawseti(L, -2, 3);
+    lua_rawseti(L, -2, pos);
+}
+
+static void
+dump_to_lua(lua_State *L, PATH_STACK *s, EXPAND_LIST *l, int w, int h, int xshift, int yshift) {
+    lua_newtable(L);
+    lua_pushinteger(L, w);
+    lua_setfield(L, -2, "w");
+    lua_pushinteger(L, h);
+    lua_setfield(L, -2, "h");
     int num = 0;
     int x, y;
     PATH_NODE *p;
     while (s->top >= 0) {
         p = s->arr[s->top--];
+        x = p->x + xshift;
+        y = p->y + yshift;
+        // printf("push = %d %d %d\n", x, y, p->dir);
+        add_lua_table(L, x, y, p->dir, ++num);
+        free(p);
+        p = NULL;
+    }
+    CANDIDATE_NODE *q;
+    if (l != NULL) {
+        for (int i = 0; i < l->num; i++) {
+            q = l->arr[i];
+            x = q->x + xshift;
+            y = q->y + yshift;
+            add_lua_table(L, x, y, q->choose_dir, ++num);
+            free(q);
+            q = NULL;
+        }
+    }
+    lua_pushinteger(L, num);
+    lua_setfield(L, -2, "num");
+}
+
+static void
+form_path(lua_State *L, PATH_STACK *s, EXPAND_LIST *l, int w, int h) {
+    int x, y, pos = s->top;
+    int min_x = w, max_x = -1, min_y = h, max_y = -1;
+    PATH_NODE *p;
+    while (pos >= 0) {
+        p = s->arr[pos--];
         x = p->x;
         y = p->y;
-        lua_newtable(L);
-        lua_pushinteger(L, x);
-        lua_rawseti(L, -2, 1);
-        lua_pushinteger(L, y);
-        lua_rawseti(L, -2, 2);
-        lua_rawseti(L, -2, ++num);
-        free(p);
+        max_x = check_max_bound(x, max_x);
+        max_y = check_max_bound(y, max_y);
+        min_x = check_min_bound(x, min_x);
+        min_y = check_min_bound(y, min_y);
+        // printf("stack trace = %d %d\n", x, y);
     }
+    if (l != NULL) {
+        printf("l num = %d \n", l->num);
+        for (int i = 0; i < l->num; i++) {
+            x = l->arr[i]->x;
+            y = l->arr[i]->y;
+            max_x = check_max_bound(x, max_x);
+            max_y = check_max_bound(y, max_y);
+            min_x = check_min_bound(x, min_x);
+            min_y = check_min_bound(y, min_y);
+            printf("l trace = %d %d\n", x, y);
+        }
+    }
+    printf("form_path = %d %d  %d  %d\n", min_x, max_x, min_y, max_y);
+    dump_to_lua(L, s, l, max_x - min_x + 1, max_y - min_y + 1, -min_x, -min_y);
+    release_expand(l);
 }
 
 static int
@@ -182,6 +457,8 @@ create_maze(lua_State *L) {
     int path_limit = getfield(L, "l");
     int sx = getfield(L, "sx");
     int sy = getfield(L, "sy");
+    int ex_num = getfield(L, "ex_num");
+    int ex_limit = getfield(L, "ex_limit");
     lua_assert(w > 0 && h > 0 && check_in_map(sx, sy, w, h));
     if (path_limit <= 0 || path_limit > w * h) {
         return 0;
@@ -189,14 +466,12 @@ create_maze(lua_State *L) {
     PATH_STACK *s = (PATH_STACK *)malloc(sizeof(PATH_STACK) + path_limit * sizeof(s->arr[0]));
     s->capacity = path_limit;
     s->top = -1;
-    PATH_NODE *first = construct(sx, sy, w, h, NULL);
+    PATH_NODE *first = construct(sx, sy, EMPTY_DIR, w, h, NULL);
     push(s, first);
-    if (path_limit == 1) {
-        back_tracking(L, s);
-    } else {
-        find_path(s, w, h);
-        back_tracking(L, s);
+    if (path_limit > 1) {
+        printf("found path = %d\n", find_path(s, w, h));
     }
+    form_path(L, s, expand(s, ex_num, ex_limit, w, h), w, h);
     free(s);
     return 1;
 }
--- test.lua
+++ test.lua
@@ -1,45 +1,103 @@
 local maze = require "maze"
 local t1 = os.clock()
 local w, h, sx, sy = 10, 10, 4, 1
-local l = 20
+local l = 84
+local ex_num, ex_limit = 1, 3
+
+local char = {
+    [-1] = 'S',
+    [0] = '↑',
+    [1] = '↓',
+    [2] = '←',
+    [3] = '→',
+}
 
 maze.set_rseed(123444)
 function get_path_idx(t, x, y)
-    for i = #t, 1, -1 do
+    for i = 1, t.num do
         if x == t[i][1] and y == t[i][2] then
-            return #t - i
+            return t.num - i
         end
     end
 end
 
-function test(w, h, sx, sy, l, i)
+function get_path_char(t, x, y)
+    for i = 1, t.num do
+        if x == t[i][1] and y == t[i][2] then
+            return t[i].dir
+        end
+    end
+end
+
+function add_dir_char(t, x, y, ch)
+    for i = 1, t.num do
+        if x == t[i][1] and y == t[i][2] then
+            if not(t[i].dir) then
+                t[i].dir = ch
+            else
+                t[i].dir = t[i].dir .. ch
+            end
+        end
+    end
+end
+
+function test(w, h, sx, sy, l, ex_num, ex_limit)
     local t = maze.create_maze({
         w = w,
         h = h,
         sx = sx,
         sy = sy,
         l = l,
+        ex_num = ex_num,
+        ex_limit = ex_limit,
     })
-    if not(t) or not(#t > 0) then
+    if not(t) or (t.num and t.num <= 0) then
         print('no found path!')
         return
+    end
+    print('======== w = ', t.w, '  h = ', t.h, '  num = ', t.num)
+    w = t.w
+    h = t.h
+    for i = 1, t.num do
+        if t[i][3] == -1 then
+            add_dir_char(t, t[i][1], t[i][2], char[-1])
+        end
+        if t[i][3] == 0 then
+            add_dir_char(t, t[i][1], t[i][2] + 1, char[0])
+        end
+        if t[i][3] == 1 then
+            add_dir_char(t, t[i][1], t[i][2] - 1, char[1])
+        end
+        if t[i][3] == 2 then
+            add_dir_char(t, t[i][1] + 1, t[i][2], char[2])
+        end
+        if t[i][3] == 3 then
+            add_dir_char(t, t[i][1] - 1, t[i][2], char[3])
+        end
     end
     local s = ""
     for i = 0, h - 1 do
         for j = 0, w - 1 do
-            local idx = get_path_idx(t, i, j)
-            if idx then
-                s = s .. idx .. ' '
+            -- local ch = get_path_char(t, j, i)
+            local ch = get_path_idx(t, j, i)
+            if ch then
+                local a = 4 - utf8.len(ch)
+                local p = ''
+                while a > 0 do
+                    p = p .. ' '
+                    a = a - 1
+                end
+                s = s .. ch .. p
             else
-                s = s .. '* '
+                s = s .. '*   '
             end
         end
         s = s .. '\n'
     end
     return s
 end
-for i = 1, 100 do
-    print(test(w, h, sx, sy, l, i))
+for i = 1, 1 do
+    print(test(w, h, sx, sy, l, ex_num, ex_limit))
 end
 
 print('cost time:', os.clock() - t1, 'ms')
